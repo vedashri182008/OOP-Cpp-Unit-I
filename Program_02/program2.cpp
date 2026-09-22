6fd4562bbc370acaedcae2aaf7b6ed13d1b3535c
vedashri182008 <deshmukhvedashri@gmail.com>
2026-09-22 19:23:10 +0530

    Create program2.cpp

---
+++ Program_02/program2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+int main()
+{
+  int marks = 45;
+  if
+    (marks >= 40)
+  { 
+    cout << "Pass"; 
+  }
+    
+  else
+  { 
+    cout << "Fail";
+  }
+  return 0;
+} 
