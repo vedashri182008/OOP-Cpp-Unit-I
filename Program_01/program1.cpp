a9936ebc6d3941ee87743393fb296cd6c5b5ff36
vedashri182008 <deshmukhvedashri@gmail.com>
2026-09-22 19:17:05 +0530

    Create program1.cpp

---
+++ Program_01/program1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int roll = 101;
+  char grade = 'A';
+  float fee = 12500.50; 
+  cout << "Roll No: " << roll << endl;
+  cout << "Grade: " << grade << endl;
+  cout << "Fee: " << fee << endl; 
+  return 0 ;
+} 
