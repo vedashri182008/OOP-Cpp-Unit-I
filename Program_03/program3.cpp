9ae954886c8f4f85cad8b08053f9c932bb4e214b
vedashri182008 <deshmukhvedashri@gmail.com>
2026-09-25 22:13:02 +0530

    Create program3.cpp

---
+++ Program_03/program3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks[5] = {78, 82, 91, 67, 88};
+
+    for (int i = 0; i < 5; i++) {
+        cout << marks[i] << " ";
+    }
+
+    return 0;
+}
